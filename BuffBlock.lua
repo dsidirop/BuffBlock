d0d36309cfd8859386d3140bc20ba39447dcc3a5
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2024-06-11 23:52:27 +0200

    feat (BuffBlock.lua): add additional slash commands /bb, /buffblock and /BuffBlock

--- BuffBlock.lua
+++ BuffBlock.lua
@@ -2,155 +2,159 @@ local BB_PlayerName = nil;
 local BB_default = {};
 local BuffBlockMenuObjects = {}
 local BuffBlockMenuStrings = {
-	[00]= "Battle Shout",
-	[01]= "Blessing of Salvation",
-	[02]= "Greater Blessing of Salvation",
-	[03]= "Divine Spirit",
-	[04]= "Prayer of Spirit",
-	[05]= "Arcane Intellect",
-	[06]= "Arcane Brilliance",
-	[07]= "Blessing of Protection"
+    [00] = "Battle Shout",
+    [01] = "Blessing of Salvation",
+    [02] = "Greater Blessing of Salvation",
+    [03] = "Divine Spirit",
+    [04] = "Prayer of Spirit",
+    [05] = "Arcane Intellect",
+    [06] = "Arcane Brilliance",
+    [07] = "Blessing of Protection"
 }
 
 function BuffBlock_OnLoad()
-	this:RegisterEvent("PLAYER_AURAS_CHANGED");
-	this:RegisterEvent("VARIABLES_LOADED");
-	DEFAULT_CHAT_FRAME:AddMessage("Buff Block, by Armilus. /BB for options", 1, 1, 0.5);
-	SLASH_BB1 = "/BB";
-	SlashCmdList["BB"] = BuffBlock_Command;
+    this:RegisterEvent("PLAYER_AURAS_CHANGED");
+    this:RegisterEvent("VARIABLES_LOADED");
+    DEFAULT_CHAT_FRAME:AddMessage("Buff Block, by Armilus. /BB for options", 1, 1, 0.5);
+    
+    SLASH_BB1 = "/BB"
+    SLASH_BB2 = "/bb"
+    SLASH_BB3 = "/buffblock"
+    SLASH_BB4 = "/BuffBlock"
+    SlashCmdList.BB = BuffBlock_Command
 end
 
 function BuffBlock_Init()
-    BB_PlayerName = UnitName("player").." of "..GetCVar("realmName");
-	
+    BB_PlayerName = UnitName("player") .. " of " .. GetCVar("realmName");
+
     if (BUFF_CONFIG == nil) then
-		BUFF_CONFIG = {};
+        BUFF_CONFIG = {};
     end
-	
-	if (BUFF_CONFIG[BB_PlayerName] == nil) then
-		BUFF_CONFIG[BB_PlayerName] = BB_default;
+
+    if (BUFF_CONFIG[BB_PlayerName] == nil) then
+        BUFF_CONFIG[BB_PlayerName] = BB_default;
     end
 end
 
 function BuffBlock_OnEvent()
-	if (event == "PLAYER_AURAS_CHANGED") then
-		Kill_Buffs();
-	elseif (event == "VARIABLES_LOADED") then
-		BuffBlock_Init();
-	end
+    if (event == "PLAYER_AURAS_CHANGED") then
+        Kill_Buffs();
+    elseif (event == "VARIABLES_LOADED") then
+        BuffBlock_Init();
+    end
 end
 
 function BuffBlock_Command()
-	if BuffBlockOptions:IsShown() then
-		BuffBlockOptions:Hide();
-	else
-		BuffBlockOptions:Show();
-	end
+    if BuffBlockOptions:IsShown() then
+        BuffBlockOptions:Hide();
+    else
+        BuffBlockOptions:Show();
+    end
 end
 
 function BuffBlock_GetOption(num)
-	local labelString = getglobal(this:GetName().."Text");
-	local label = BuffBlockMenuStrings[num] or "";
-	BuffBlockMenuObjects[num] = this
-	
-	if num == 00 and BUFF_CONFIG[BB_PlayerName].BATTLESHOUT
-	or num == 01 and BUFF_CONFIG[BB_PlayerName].SALVATION
-	or num == 02 and BUFF_CONFIG[BB_PlayerName].GREATERSALVATION
-	or num == 03 and BUFF_CONFIG[BB_PlayerName].DIVINESPIRIT
-	or num == 04 and BUFF_CONFIG[BB_PlayerName].PRAYEROFSPIRIT
-	or num == 05 and BUFF_CONFIG[BB_PlayerName].ARCANEINTELLECT
-	or num == 06 and BUFF_CONFIG[BB_PlayerName].ARCANEBRILLIANCE
-	or num == 07 and BUFF_CONFIG[BB_PlayerName].BLESSINGOFPROTECTION
-	or nil then
-		this:SetChecked(true);
-	else
-		this:SetChecked(nil);
-	end
-	labelString:SetText(label);
+    local labelString = getglobal(this:GetName() .. "Text");
+    local label = BuffBlockMenuStrings[num] or "";
+    BuffBlockMenuObjects[num] = this
+
+    if num == 00 and BUFF_CONFIG[BB_PlayerName].BATTLESHOUT
+            or num == 01 and BUFF_CONFIG[BB_PlayerName].SALVATION
+            or num == 02 and BUFF_CONFIG[BB_PlayerName].GREATERSALVATION
+            or num == 03 and BUFF_CONFIG[BB_PlayerName].DIVINESPIRIT
+            or num == 04 and BUFF_CONFIG[BB_PlayerName].PRAYEROFSPIRIT
+            or num == 05 and BUFF_CONFIG[BB_PlayerName].ARCANEINTELLECT
+            or num == 06 and BUFF_CONFIG[BB_PlayerName].ARCANEBRILLIANCE
+            or num == 07 and BUFF_CONFIG[BB_PlayerName].BLESSINGOFPROTECTION
+            or nil then
+        this:SetChecked(true);
+    else
+        this:SetChecked(nil);
+    end
+    labelString:SetText(label);
 end
 
 function BuffBlock_SetOption(num)
-	local checked = this:GetChecked()
-	if num == 00 then
-		if checked then
-			BUFF_CONFIG[BB_PlayerName].BATTLESHOUT = 1
-			DEFAULT_CHAT_FRAME:AddMessage("Blocking "..BuffBlockMenuStrings[num]);
-		else
-			BUFF_CONFIG[BB_PlayerName].BATTLESHOUT = nil
-			DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking "..BuffBlockMenuStrings[num]);
-		end
-	elseif num == 01 then
-		if checked then
-			BUFF_CONFIG[BB_PlayerName].SALVATION = 1
-			DEFAULT_CHAT_FRAME:AddMessage("Blocking "..BuffBlockMenuStrings[num]);
-		else
-			BUFF_CONFIG[BB_PlayerName].SALVATION = nil
-			DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking "..BuffBlockMenuStrings[num]);
-		end
-	elseif num == 02 then
-		if checked then
-			BUFF_CONFIG[BB_PlayerName].GREATERSALVATION = 1
-			DEFAULT_CHAT_FRAME:AddMessage("Blocking "..BuffBlockMenuStrings[num]);
-		else
-			BUFF_CONFIG[BB_PlayerName].GREATERSALVATION = nil
-			DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking "..BuffBlockMenuStrings[num]);
-		end
-	elseif num == 03 then
-		if checked then
-			BUFF_CONFIG[BB_PlayerName].DIVINESPIRIT = 1
-			DEFAULT_CHAT_FRAME:AddMessage("Blocking "..BuffBlockMenuStrings[num]);
-		else
-			BUFF_CONFIG[BB_PlayerName].DIVINESPIRIT = nil
-			DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking "..BuffBlockMenuStrings[num]);
-		end
-	elseif num == 04 then
-		if checked then
-			BUFF_CONFIG[BB_PlayerName].PRAYEROFSPIRIT = 1
-			DEFAULT_CHAT_FRAME:AddMessage("Blocking "..BuffBlockMenuStrings[num]);
-		else
-			BUFF_CONFIG[BB_PlayerName].PRAYEROFSPIRIT = nil
-			DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking "..BuffBlockMenuStrings[num]);
-		end
-	elseif num == 05 then
-		if checked then
-			BUFF_CONFIG[BB_PlayerName].ARCANEINTELLECT = 1
-			DEFAULT_CHAT_FRAME:AddMessage("Blocking "..BuffBlockMenuStrings[num]);
-		else
-			BUFF_CONFIG[BB_PlayerName].ARCANEINTELLECT = nil
-			DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking "..BuffBlockMenuStrings[num]);
-		end
-	elseif num == 06 then
-		if checked then
-			BUFF_CONFIG[BB_PlayerName].ARCANEBRILLIANCE = 1
-			DEFAULT_CHAT_FRAME:AddMessage("Blocking "..BuffBlockMenuStrings[num]);
-		else
-			BUFF_CONFIG[BB_PlayerName].ARCANEBRILLIANCE = nil
-			DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking "..BuffBlockMenuStrings[num]);
-		end
-	elseif num == 07 then
-		if checked then
-			BUFF_CONFIG[BB_PlayerName].BLESSINGOFPROTECTION = 1
-			DEFAULT_CHAT_FRAME:AddMessage("Blocking "..BuffBlockMenuStrings[num]);
-		else
-			BUFF_CONFIG[BB_PlayerName].BLESSINGOFPROTECTION = nil
-			DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking "..BuffBlockMenuStrings[num]);
-		end
-	end
+    local checked = this:GetChecked()
+    if num == 00 then
+        if checked then
+            BUFF_CONFIG[BB_PlayerName].BATTLESHOUT = 1
+            DEFAULT_CHAT_FRAME:AddMessage("Blocking " .. BuffBlockMenuStrings[num]);
+        else
+            BUFF_CONFIG[BB_PlayerName].BATTLESHOUT = nil
+            DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking " .. BuffBlockMenuStrings[num]);
+        end
+    elseif num == 01 then
+        if checked then
+            BUFF_CONFIG[BB_PlayerName].SALVATION = 1
+            DEFAULT_CHAT_FRAME:AddMessage("Blocking " .. BuffBlockMenuStrings[num]);
+        else
+            BUFF_CONFIG[BB_PlayerName].SALVATION = nil
+            DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking " .. BuffBlockMenuStrings[num]);
+        end
+    elseif num == 02 then
+        if checked then
+            BUFF_CONFIG[BB_PlayerName].GREATERSALVATION = 1
+            DEFAULT_CHAT_FRAME:AddMessage("Blocking " .. BuffBlockMenuStrings[num]);
+        else
+            BUFF_CONFIG[BB_PlayerName].GREATERSALVATION = nil
+            DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking " .. BuffBlockMenuStrings[num]);
+        end
+    elseif num == 03 then
+        if checked then
+            BUFF_CONFIG[BB_PlayerName].DIVINESPIRIT = 1
+            DEFAULT_CHAT_FRAME:AddMessage("Blocking " .. BuffBlockMenuStrings[num]);
+        else
+            BUFF_CONFIG[BB_PlayerName].DIVINESPIRIT = nil
+            DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking " .. BuffBlockMenuStrings[num]);
+        end
+    elseif num == 04 then
+        if checked then
+            BUFF_CONFIG[BB_PlayerName].PRAYEROFSPIRIT = 1
+            DEFAULT_CHAT_FRAME:AddMessage("Blocking " .. BuffBlockMenuStrings[num]);
+        else
+            BUFF_CONFIG[BB_PlayerName].PRAYEROFSPIRIT = nil
+            DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking " .. BuffBlockMenuStrings[num]);
+        end
+    elseif num == 05 then
+        if checked then
+            BUFF_CONFIG[BB_PlayerName].ARCANEINTELLECT = 1
+            DEFAULT_CHAT_FRAME:AddMessage("Blocking " .. BuffBlockMenuStrings[num]);
+        else
+            BUFF_CONFIG[BB_PlayerName].ARCANEINTELLECT = nil
+            DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking " .. BuffBlockMenuStrings[num]);
+        end
+    elseif num == 06 then
+        if checked then
+            BUFF_CONFIG[BB_PlayerName].ARCANEBRILLIANCE = 1
+            DEFAULT_CHAT_FRAME:AddMessage("Blocking " .. BuffBlockMenuStrings[num]);
+        else
+            BUFF_CONFIG[BB_PlayerName].ARCANEBRILLIANCE = nil
+            DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking " .. BuffBlockMenuStrings[num]);
+        end
+    elseif num == 07 then
+        if checked then
+            BUFF_CONFIG[BB_PlayerName].BLESSINGOFPROTECTION = 1
+            DEFAULT_CHAT_FRAME:AddMessage("Blocking " .. BuffBlockMenuStrings[num]);
+        else
+            BUFF_CONFIG[BB_PlayerName].BLESSINGOFPROTECTION = nil
+            DEFAULT_CHAT_FRAME:AddMessage("Stopped blocking " .. BuffBlockMenuStrings[num]);
+        end
+    end
 end
 
 function IsShieldEquipped()
-	local slot = GetInventorySlotInfo("SecondaryHandSlot")
-	local link = GetInventoryItemLink("player", slot)
-	if link  then
-		local found, _, id, name = string.find(link, "item:(%d+):.*%[(.*)%]")
-		if found then
-			local _,_,_,_,_,itemType = GetItemInfo(tonumber(id))
-			if(itemType == "Shields") then
-				return true
-			end
-		end
-	end
-	return false
+    local slot = GetInventorySlotInfo("SecondaryHandSlot")
+    local link = GetInventoryItemLink("player", slot)
+    if link then
+        local found, _, id, name = string.find(link, "item:(%d+):.*%[(.*)%]")
+        if found then
+            local _, _, _, _, _, itemType = GetItemInfo(tonumber(id))
+            if (itemType == "Shields") then
+                return true
+            end
+        end
+    end
+    return false
 end
 
 function Kill_Buffs()
